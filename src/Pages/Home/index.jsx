6fb793b5d4a8b382d8402230731bddfd2a679bca
lingowmx <clingow@hotmail.com>
2024-07-08 13:00:02 -0500

    Search input working. bug detected in the thick button, need to add a toggle there

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,28 +5,39 @@ import { Card } from "../../components/Card";
 import { ProductDetail } from "../../components/ProductDetail";
 
 export const Home = () => {
-    const {items, setSearchByTitle} = useContext(ShoppingCartContext)
+  const { items, setSearchByTitle, filteredItems, searchByTitle } =
+    useContext(ShoppingCartContext);
+  const renderView = () => {
+    const itemsToRender = searchByTitle?.length > 0 ? filteredItems : items;
+    if (itemsToRender?.length > 0) {
+      return itemsToRender.map((item) => (
+         <Card key={item.id} {...item} />
+      ));
+    } else {
+      return <p>No Results, sorry</p>;
+    }
+  };
 
   return (
     <Layout>
       <div>
         <h1>HOME</h1>
       </div>
-      <input type="text" 
-      placeholder="Search a product"
-      className=" border border-black rounded-lg w-48 p-4 my-4 focus:outline-none" 
-      onChange={(event) => setSearchByTitle(event.target.value)}/>
+      <input
+        type="text"
+        placeholder="Search a product"
+        className=" border border-black rounded-lg w-48 p-4 my-4 focus:outline-none"
+        onChange={(event) => setSearchByTitle(event.target.value)}
+      />
       <div className="w-full grid place-content-center justify-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-          {
-            //PENDIENTE PARA REFACTORIZAR , Se refactorizo y se aplico el uso de proptypes
-            //PENDIENTE lazy loading
-            //PROP TYPES. -listo
-            items?.map((item) => {
-              return <Card key={item.id} {...item} />;
-            })
-          }
-      </div>  
-      <ProductDetail/>
+        {
+          //PENDIENTE PARA REFACTORIZAR , Se refactorizo y se aplico el uso de proptypes
+          //PENDIENTE lazy loading
+          //PROP TYPES. -listo
+          renderView()
+        }
+      </div>
+      <ProductDetail />
     </Layout>
   );
 };
--- src/components/Context/index.jsx
+++ src/components/Context/index.jsx
@@ -2,9 +2,9 @@ import { createContext } from "react";
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-export const ShoppingCartContext = createContext(); //crear un contexto, 
-export const ShoppingCartProvider = ({ children }) => {//crear proveedor que encapsula
-
+export const ShoppingCartContext = createContext(); //crear un contexto,
+export const ShoppingCartProvider = ({ children }) => {
+  //crear proveedor que encapsula
 
   const [count, setCount] = useState(0);
   //Product Detail Open/Close
@@ -13,24 +13,38 @@ export const ShoppingCartProvider = ({ children }) => {//crear proveedor que enc
   const closeProductDetail = () => SetIsProductDetailOpen(false);
   //Product Detail
   const [sendProduct, setSendProduct] = useState({});
-    //ShoppingCart
+  //ShoppingCart
   const [productsToBuy, setProductsToBuy] = useState([]);
   //Shopping Cart Open/Close
   const [isCheckOutMenuOpen, SetIsCheckOutMenuOpen] = useState(false);
   const openCheckOutMenu = () => SetIsCheckOutMenuOpen(true);
   const closeCheckOutMenu = () => SetIsCheckOutMenuOpen(false);
-//order
-  const [order, setOrder] = useState([])
+  //order
+  const [order, setOrder] = useState([]);
   // products
   const [items, setItems] = useState(null);
+  // get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null);
+  console.log("searchByTitle:", searchByTitle);
+  const [filteredItems, setFilteredItems] = useState(null);
+
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((response) => response.json())
       .then((data) => setItems(data));
   }, []);
-  // get products by title
-  const [searchByTitle, setSearchByTitle] = useState(null)
-  console.log('searchByTitle:', searchByTitle)
+  console.log("filteredItems:", filteredItems);
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter((item) =>
+      item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+    );
+  };
+  useEffect(() => {
+    if (searchByTitle) {
+      setFilteredItems(filteredItemsByTitle(items, searchByTitle));
+    }
+  }, [items, searchByTitle]);
   return (
     <ShoppingCartContext.Provider
       value={{
@@ -46,13 +60,14 @@ export const ShoppingCartProvider = ({ children }) => {//crear proveedor que enc
         isCheckOutMenuOpen,
         SetIsCheckOutMenuOpen,
         openCheckOutMenu,
-        closeCheckOutMenu ,
+        closeCheckOutMenu,
         order,
         setOrder,
         items,
         setItems,
         searchByTitle,
-      setSearchByTitle
+        setSearchByTitle,
+        filteredItems
       }}
     >
       {children}
@@ -62,5 +77,5 @@ export const ShoppingCartProvider = ({ children }) => {//crear proveedor que enc
 
 //PROPTYPES
 ShoppingCartProvider.propTypes = {
-    children:PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
